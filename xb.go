c55b6c052c889e97226eea643a3d21020570abee
James Liu <jamesliu96@users.noreply.github.com>
2022-01-13 11:43:59 +0800

    feat: decode

--- xb.go
+++ xb.go
@@ -7,5 +7,13 @@ import (
 )
 
 func main() {
-	io.Copy(os.Stdout, hex.NewDecoder(os.Stdin))
+	if len(os.Args) > 1 {
+		if _, err := io.Copy(os.Stdout, hex.NewDecoder(os.Stdin)); err != nil {
+			panic(err)
+		}
+		return
+	}
+	if _, err := io.Copy(hex.NewEncoder(os.Stdout), os.Stdin); err != nil {
+		panic(err)
+	}
 }
